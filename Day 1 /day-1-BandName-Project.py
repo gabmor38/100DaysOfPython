0e76e8148353b66a8714af181d17974b9af94d8d
gabmor38 <gabriela.morenor@gmail.com>
2024-08-07 10:05:54 -0400

    initial commit

---
+++ Day 1 /day-1-BandName-Project.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("Which city did you grew up in?\n")
+petName = input("What is the name of your pet?\n")
+print("Your band name is :", city + petName)
