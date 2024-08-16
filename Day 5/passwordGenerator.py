853d9aed6075d79c74fe5dfe0d2608c8e607bc86
gabmor38 <gabriela.morenor@gmail.com>
2024-08-16 07:50:33 -0400

    created second solution

--- Day 5/passwordGenerator.py
+++ Day 5/passwordGenerator.py
@@ -8,6 +8,8 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+#solution 1
+
 letter = random.sample(letters, nr_letters)
 symbols = random.sample(symbols,  nr_symbols)
 numbers = random.sample(numbers, nr_numbers)
@@ -16,3 +18,23 @@ password = letter + symbols + numbers
 random.shuffle(password)
 
 print(''.join(password))
+
+
+#solution 2
+
+passwordList = []
+
+for char in range(0, nr_letters):
+    passwordList += random.choice(letters)
+    print(passwordList)
+
+for char in range(0, nr_symbols):
+    passwordList += random.choice(symbols)
+    print(passwordList)
+
+for char in range(0, nr_numbers):
+    passwordList += random.choice(numbers)
+
+random.shuffle(passwordList)
+print(''.join(passwordList))
+
